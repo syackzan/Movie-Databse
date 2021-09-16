098040297c01e77e234d39f044f3d1a76f0f3f25
Scotty Yackzan <syackzan@gmail.com>
2021-09-16 11:39:17 -0700

    Added Query Selectors to the server.js file - missing new movie

--- db/query.sql
+++ db/query.sql
@@ -6,13 +6,11 @@ INSERT INTO movies (title) VALUES (title);
 INSERT INTO reviews (review) VALUES (review);
 
 -- Query to Update a Movie --
-UPDATE movies SET title = value1 WHERE id = ?;
+UPDATE reviews SET review = ? WHERE id = ?;
 
 -- Query to Delete a Movie --
 DELETE FROM movies WHERE id = ?;
 DELETE FROM reviews WHERE id = ?;
 
 -- Query to Render Movies w/ their Reviews --
-SELECT movies.title AS Title, reviews.review AS Reviews
-FROM reviews
-JOIN movies ON reviews.movies_id = movies.id;
+SELECT movies.title AS Title, reviews.review AS Reviews FROM reviews JOIN movies ON reviews.movies_id = movies.id;
