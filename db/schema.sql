6625a6357d3145ae649dd12462523b3d4e79a7ff
Scotty Yackzan <syackzan@gmail.com>
2021-09-16 10:26:10 -0700

    Added tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,15 @@ CREATE DATABASE movies_db;
 USE movies_db;
 
 CREATE TABLE movies (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE reviews (
-    
-)
+    id INT PRIMARY KEY,
+    review TEXT NOT NULL,
+    movies_id INT,
+    FOREIGN KEY (movies_id)
+    REFERENCES movies(id)
+    ON DELETE SET NULL
+);
