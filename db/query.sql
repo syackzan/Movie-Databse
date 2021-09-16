1ed588fb45d2fa2263eb22a5fe425f59fb29bf42
Scotty Yackzan <syackzan@gmail.com>
2021-09-16 10:54:56 -0700

    Added Query's

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+-- Query to Render All Movies --
+SELECT * FROM movies;
+
+-- Query to Insert Movie --
+INSERT INTO movies (title) VALUES (title);
+
+-- Query to Update a Movie --
+UPDATE movies SET title = value1 WHERE id = ?;
+
+-- Query to Delete a Movie --
+DELETE FROM movies WHERE id = ?;
